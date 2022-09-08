f98bf6058db7d7e2c086521b296754984f982816
Sergey Kazmin <kazmin5796@mail.ru>
2022-09-08 14:14:58 +0300

    Add RestControllers

---
+++ BootStrap-master/Security-master/PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/controllers/RestAdminController.java
@@ -0,0 +1,42 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class RestAdminController {
+    private final UserService userService;
+
+    @Autowired
+    public RestAdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public List<User> printUsers() {
+        return userService.getUsers();
+    }
+    @PostMapping()
+    public ResponseEntity<HttpStatus> createUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<HttpStatus> updateUser(@RequestBody User user, @PathVariable("id") Long id) {
+        user.setId(id);
+        userService.updateUser(user);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+}
---
+++ BootStrap-master/Security-master/PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/controllers/RestUserController.java
@@ -0,0 +1,20 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.kata.spring.boot_security.demo.models.User;
+
+
+@RestController
+@RequestMapping("/user")
+public class RestUserController {
+    @GetMapping()
+    public User showUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return (User) authentication.getPrincipal();
+    }
+}
